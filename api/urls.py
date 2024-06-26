5e082fe920793290964f0b0b64a603e8fb474c06
Emmanuel Cyril <cyrile450@gmail.com>
2024-06-26 06:35:16 -0700

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     # path('', views.home, name='home'),
     path('auth/register/', views.register, name='register'),
-    path('/auth/resend-verify-email/', views.resend_verify_email, name='resend-verify-email'),
+    path('auth/resend-verify-email/', views.resend_verify_email, name='resend-verify-email'),
     path('auth/verify-email/', views.verify_email, name='verify_email'),
     path('auth/sign-in/', views.login, name='login'),
     path('logout/', views.logout, name="logout"),
