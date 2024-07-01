0828ccd2f95e46589674f04a0bad57e01e13ed4d
Myles181 <cyrile450@gmail.com>
2024-07-01 15:17:25 -0700

    api

--- scuibai/settings.py
+++ scuibai/settings.py
@@ -109,6 +109,15 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+REST_FRAMEWORK = {
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.TokenAuthentication',
+   ),
+   'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAdminUser'
+   ),
+}
+
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
