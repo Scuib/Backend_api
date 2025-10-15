90b64d0b7fc7cd3335041b53ca89cdb705bb4bec
Godwin Phillips Okpe <okpegodwin18@yahoo.com>
2025-10-15 08:12:23 +0100

    Fixed email capitalisation error

--- api/managers.py
+++ api/managers.py
@@ -9,16 +9,16 @@ class CustomUserManager(BaseUserManager):
             validate_email(email)
         except ValidationError:
             raise ValueError(_("You must provide a valid email"))
-    
+
     def create_user(self, email, first_name, last_name=None, password=None, **extra_fields):
         if not email:
             raise ValueError(_("This is required field"))
         else:
             self.email_validation(email)
-            clean_email = self.normalize_email(email)
+            clean_email = self.normalize_email(email).lower().strip()
         if not first_name:
             raise ValueError(_("This is required field"))
-        
+
         user = self.model(
             email = clean_email,
             first_name = first_name,
@@ -30,7 +30,7 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault("is_staff", False)
         user.save()
         return user
-    
+
     def create_superuser(self, email, first_name, last_name=None, password=None, **extra_fields):
         extra_fields.setdefault("is_active", True)
         extra_fields.setdefault("is_superuser", True)
@@ -40,14 +40,14 @@ class CustomUserManager(BaseUserManager):
             raise ValueError(_("This is required field"))
         else:
             self.email_validation(email)
-            clean_email = self.normalize_email(email)
+            clean_email = self.normalize_email(email).lower().strip()
         if not first_name:
             raise ValueError(_("This is required field"))
         if extra_fields.get("is_superuser") is not True:
             raise ValueError(_("Superuser must have True value in is_superuser field"))
         if extra_fields.get("is_staff") is not True:
             raise ValueError(_("Superuser must have True value in is_staff field"))
-        
+
         user = self.create_user(clean_email, first_name, last_name, password, **extra_fields)
         user.save()
         return user
